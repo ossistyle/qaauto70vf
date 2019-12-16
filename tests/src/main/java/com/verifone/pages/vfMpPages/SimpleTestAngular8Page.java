2352b6b04e8a8447118c24e94d0be83abe5f0901
SergeyV1 <sergey.vinickiy@verifone.com>
2019-12-16 16:53:17 +0200

    vfmp changes

--- tests/src/main/java/com/verifone/pages/vfMpPages/SimpleTestAngular8Page.java
+++ tests/src/main/java/com/verifone/pages/vfMpPages/SimpleTestAngular8Page.java
@@ -15,9 +15,6 @@ import java.io.File;
 
 public class SimpleTestAngular8Page extends BasePage {
 
-//    private final static String url = "http://dev.merchant.verifonecp.com/home";
-//    private final static String title = "Verifone-Marketplace";
-
     private final static String url = BaseTest.envConfig.getWebUrl();
     private final static String title = "Verifone-Marketplace";
 
@@ -27,6 +24,18 @@ public class SimpleTestAngular8Page extends BasePage {
     private By btnSettings = ByAngular.buttonText("Settings");
     private By btnUsers = ByAngularBinding.partialLinkText("Users");
     private By clickImg = ByAngularBinding.xpath("//img[@src='https://images.unsplash.com/photo-1506919258185-6078bba55d2a?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb']");
+    private By btnApplications = ByAngular.buttonText("Applications");
+    private By Applications = ByAngularBinding.partialLinkText("Applications");
+    private By appPage1 = ByAngularBinding.xpath("//button[@class= 'button card-footer-item is-link modal-button'][1]");
+    private By appPage2 = ByAngularBinding.xpath("//button[@class= 'button card-footer-item is-link modal-button'][2]");
+    private By appPage3 = ByAngularBinding.xpath("//button[@class= 'button card-footer-item is-link modal-button'][3]");
+    private By appPage4 = ByAngularBinding.xpath("//button[@class= 'button card-footer-item is-link modal-button'][4]");
+    private By quickView1 = ByAngularBinding.xpath("//button[@class='button card-footer-item  '][1]");
+
+
+
+    private By btnCalculate = ByAngular.buttonText("Calculate!");
+
 
 
     private By firstUsername = By.id("username");
@@ -74,19 +83,58 @@ public class SimpleTestAngular8Page extends BasePage {
 
 
     //angular verification- poc
-    public void testAngular() {
+    public void testAngular() throws Exception {
         NgWebDriver ngDriver = new NgWebDriver((JavascriptExecutor) getDriver());
         ngDriver.waitForAngularRequestsToFinish();
 
-        testLog.info("---------------------------- Navigate to App Catalog ---------------------------------");
-        click(btnAppCatalog);
 
+        System.out.println("click Application");
+        testLog.info("---------------------------- click Applications ---------------------------------");
+        click(btnApplications);
+
+
+        waitForLoader(appPage1);
+        click(quickView1);
+
+        Thread.sleep(3000);
+        click(appPage1);
+
+       // click(btnApplications);
+
+
+//        click(btnApplications);
+//        waitForLoader(appPage1);
+//        click(appPage2);
+//
+//        click(btnApplications);
+//        waitForLoader(appPage1);
+//        click(appPage3);
+//
+//        click(btnApplications);
+//        waitForLoader(appPage1);
+//        click(appPage4);
+
+
+//        ngDriver.waitForAngularRequestsToFinish();
+//        waitForLoader(btnAppPage);
+//
+//        testLog.info("---------------------------- click Calculate! ---------------------------------");
+//        click(btnAppPage);
+//        scrollToElement(btnCalculate);
+
+//        System.out.println("click Calculate");
+//        testLog.info("---------------------------- Navigate to Calculate ---------------------------------");
+//        click(btnCalculate);
+
+        System.out.println("click Settings");
         testLog.info("---------------------------- Navigate to Settings ---------------------------------");
         click(btnSettings);
 
+        System.out.println("click Users");
         testLog.info("---------------------------- Navigate to Users ---------------------------------");
         click(btnUsers);
 
+        System.out.println("click Image");
         testLog.info("---------------------------- Click on Image ---------------------------------");
         click(clickImg);
         driver.close();
