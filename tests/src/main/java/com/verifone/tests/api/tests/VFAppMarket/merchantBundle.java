2cfcf4a472f809d6e4ef1ad3a6fe5cac660fca8b
SergeyV1 <sergey.vinickiy@verifone.com>
2019-12-16 16:43:51 +0200

    evaluate bundle

--- tests/src/main/java/com/verifone/tests/api/tests/VFAppMarket/merchantBundle.java
+++ tests/src/main/java/com/verifone/tests/api/tests/VFAppMarket/merchantBundle.java
@@ -23,12 +23,31 @@ public class merchantBundle extends BaseTest {
         file = setFilePath("noFilehere.xls", "merchant-bundleVFMP.xls");
     }
 
-    @DataProvider(name = "bundles list")
+/*  @DataProvider(name = "bundles list")
     public Object[][] location() throws Exception {
         Object[][] arrayObject = DataDrivenUtils.getExcelData(file, "returnBundles-gvcca2319");
         return arrayObject;
     }
 
+    @DataProvider(name = "negative_test")
+    public Object[][] negative_test() throws Exception {
+        Object[][] arrayObject = DataDrivenUtils.getExcelData(file, "NegativeTest");
+        return arrayObject;
+    }
+
+    @DataProvider(name = "freeAppsBundle")
+    public Object[][] location() throws Exception {
+        Object[][] arrayObject = DataDrivenUtils.getExcelData(file, "freeapps");
+        return arrayObject;
+    }   */
+
+    @DataProvider(name = "existing_assignment")
+    public Object[][] location() throws Exception {
+        Object[][] arrayObject = DataDrivenUtils.getExcelData(file, "existing_assignment");
+        return arrayObject;
+    }
+
+/*
     @Test(dataProvider = "bundles list", groups = "cloudApi1")
 
     public void cloudApiLocationDDT(String accessToken, String accGrantType, String accSSOURL, String uri, String requestMethod,
@@ -40,4 +59,42 @@ public class merchantBundle extends BaseTest {
         offerId = api.startProsess_ValidateExcludeDataEvaluaet(accessToken, accGrantType, accSSOURL, uri, requestMethod, headers, headersForGetToken, body,
                 expectedStatusCode, expectedResult, verifyList,verifyExcludeList, offerId, rowNum );
     }
+
+
+    @Test(dataProvider = "negative_test", groups = "cloudApi1")
+
+    public void negativeTestDDT(String accessToken, String accGrantType, String accSSOURL, String uri, String requestMethod,
+                                    String headers, String headersForGetToken, String body, String expectedStatusCode,
+                                    String expectedResult, String verifyList,String verifyExcludeList, String comments, String rowNum) throws Exception {
+        starTestLog(rowNum + ". " + comments, comments);
+
+        DataDrivenApi api = new DataDrivenApi((ExtentTest) test.get(),false);
+        offerId = api.startProsess_ValidateExcludeDataEvaluaet(accessToken, accGrantType, accSSOURL, uri, requestMethod, headers, headersForGetToken, body,
+                expectedStatusCode, expectedResult, verifyList,verifyExcludeList, offerId, rowNum );
+    }
+
+    @Test(dataProvider = "freeAppsBundle", groups = "cloudApi1")
+
+    public void freeBundleDDT(String accessToken, String accGrantType, String accSSOURL, String uri, String requestMethod,
+                                String headers, String headersForGetToken, String body, String expectedStatusCode,
+                                String expectedResult, String verifyList,String verifyExcludeList, String comments, String rowNum) throws Exception {
+        starTestLog(rowNum + ". " + comments, comments);
+
+        DataDrivenApi api = new DataDrivenApi((ExtentTest) test.get(),false);
+        offerId = api.startProsess_ValidateExcludeDataEvaluaet(accessToken, accGrantType, accSSOURL, uri, requestMethod, headers, headersForGetToken, body,
+                expectedStatusCode, expectedResult, verifyList,verifyExcludeList, offerId, rowNum );
+    }   */
+@Test(dataProvider = "existing_assignment", groups = "cloudApi1")
+
+public void existingAssignmentDDT(String accessToken, String accGrantType, String accSSOURL, String uri, String requestMethod,
+                          String headers, String headersForGetToken, String body, String expectedStatusCode,
+                          String expectedResult, String verifyList,String verifyExcludeList, String comments, String rowNum) throws Exception {
+    starTestLog(rowNum + ". " + comments, comments);
+
+    DataDrivenApi api = new DataDrivenApi((ExtentTest) test.get(),false);
+    offerId = api.startProsess_ValidateExcludeDataEvaluaet(accessToken, accGrantType, accSSOURL, uri, requestMethod, headers, headersForGetToken, body,
+            expectedStatusCode, expectedResult, verifyList,verifyExcludeList, offerId, rowNum );
+}
+
+
 }
