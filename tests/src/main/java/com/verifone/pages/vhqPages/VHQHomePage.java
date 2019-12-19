80094093c603ca4484808bd8197877ae0a0f864d
Viacheslav Dontsov <viacheslavd1@verifone.com>
2019-12-19 14:50:12 +0200

    Fix junit asserts

--- tests/src/main/java/com/verifone/pages/vhqPages/VHQHomePage.java
+++ tests/src/main/java/com/verifone/pages/vhqPages/VHQHomePage.java
@@ -1,9 +1,12 @@
 package com.verifone.pages.vhqPages;
 
 import com.verifone.pages.BasePage;
+import com.verifone.pages.mpPages.CBAAssignPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.SkipException;
+
 import java.net.InetAddress;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
