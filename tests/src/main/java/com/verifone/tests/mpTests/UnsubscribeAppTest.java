6507dad7c906edfaf1b7824c21451bda9401cdab
Prashant <t_prashantl1@verifone.com>
2019-12-20 10:42:24 +0530

    Changes only related to appending prefix at the end of mppages name

--- tests/src/main/java/com/verifone/tests/mpTests/UnsubscribeAppTest.java
+++ tests/src/main/java/com/verifone/tests/mpTests/UnsubscribeAppTest.java
@@ -1,11 +1,8 @@
 package com.verifone.tests.mpTests;
 
-import com.verifone.entities.EntitiesFactory;
-import com.verifone.infra.User;
 import com.verifone.pages.PageFactory;
-import com.verifone.pages.mpPages.CBAAccount;
+import com.verifone.pages.mpPages.CBAAccountPage;
 import com.verifone.tests.BaseTest;
-import com.verifone.tests.steps.mpPortal.Steps;
 import org.testng.annotations.Test;
 
 import static com.verifone.tests.steps.mpPortal.Steps.*;
@@ -20,7 +17,7 @@ public class UnsubscribeAppTest extends BaseTest {
         //loginCBA(createMerchantUser());
         loginCBA(createAssignUser());
         getAppName = BaseTest.envConfig.getAppName();
-        CBAAccount account = PageFactory.getCBAAccount();
+        CBAAccountPage account = PageFactory.getCBAAccount();
         account.cancelSubscribsion(getAppName);
 
     }
