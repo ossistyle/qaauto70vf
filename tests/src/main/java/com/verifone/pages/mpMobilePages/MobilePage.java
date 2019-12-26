09dc65b00437208b7b0feff56ff16f6c46534440
Viacheslav Dontsov <viacheslavd1@verifone.com>
2019-12-26 14:08:03 +0200

    Appium integration

--- tests/src/main/java/com/verifone/pages/mpMobilePages/MobilePage.java
+++ tests/src/main/java/com/verifone/pages/mpMobilePages/MobilePage.java
@@ -1,15 +1,11 @@
 package com.verifone.pages.mpMobilePages;
 
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
 import com.verifone.pages.BasePage;
 import com.verifone.pages.mpMobilePages.components.Header;
 import com.verifone.pages.mpMobilePages.components.MainMenu;
-import io.appium.java_client.android.AndroidDriver;
 
 public abstract class MobilePage extends BasePage {
 
-    protected AndroidDriver<SelenideElement> androidDriver;
     private static final String URL = null;
     private static final String TITLE = "Ionic App";
 
@@ -18,8 +14,6 @@ public abstract class MobilePage extends BasePage {
 
     public MobilePage() {
         super(URL, TITLE);
-        this.androidDriver = WebDriverRunner.hasWebDriverStarted()
-                ? (AndroidDriver<SelenideElement>) WebDriverRunner.getAndCheckWebDriver() : null;
         this.header = new Header();
         this.mainMenu = new MainMenu();
     }
