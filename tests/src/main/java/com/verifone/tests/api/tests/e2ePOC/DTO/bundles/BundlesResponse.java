c7e17ab2fcaccc119ddba2c2282981354e2d8091
schura <schura@exadel.com>
2020-01-19 17:13:17 +0300

    GVCCA-1685 - added negative tests, split long scenario to a one test

--- tests/src/main/java/com/verifone/tests/api/tests/e2ePOC/DTO/bundles/BundlesResponse.java
+++ tests/src/main/java/com/verifone/tests/api/tests/e2ePOC/DTO/bundles/BundlesResponse.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class BundlesResponse {
     private String status;
     private List<BundlesData> data;
+    private List<BundleError> errors;
 
     public String getStatus() {
         return status;
@@ -20,5 +21,13 @@ public class BundlesResponse {
 
     public void setData(List<BundlesData> data) {
         this.data = data;
+    }
+
+    public List<BundleError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<BundleError> errors) {
+        this.errors = errors;
     }
 }
