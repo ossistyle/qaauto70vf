c699407b5aee0e2485b4733588c28e2473d8a38f
schura <schura@exadel.com>
2020-01-20 20:45:46 +0300

    GVCCA-1685 -added asserts, renamed classes.

--- tests/src/main/java/com/verifone/tests/api/tests/e2ePOC/tests/BundlesNegativeTests.java
+++ tests/src/main/java/com/verifone/tests/api/tests/e2ePOC/tests/BundlesNegativeTests.java
@@ -7,17 +7,19 @@ import com.verifone.tests.api.tests.e2ePOC.DTO.bundles.BundlesResponse;
 import com.verifone.tests.api.tests.e2ePOC.DTO.internalCustomObjects.ApiResponse;
 import com.verifone.tests.api.tests.e2ePOC.helpers.LoginHelper;
 import com.verifone.tests.api.tests.e2ePOC.helpers.api.BundlesApiHandler;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class PocNegativeTest extends BaseTest {
+public class BundlesNegativeTests extends BaseTest {
 
     private String createdBundleId;
     private BundlesApiHandler bundlesHandler;
     private Gson jsonParser = new Gson();
     private String env;
+    private String eoToken;
 
 
     @BeforeClass
@@ -25,21 +27,21 @@ public class PocNegativeTest extends BaseTest {
     public void createBundle(String env) throws Exception {
         this.env = env;
         Gson jsonPaser = new Gson();
-        String eoToken = LoginHelper.getRequestToken(env, "vfameo@getnada.com", "Veri1234");
+        eoToken = LoginHelper.getRequestToken(env, "vfameo@getnada.com", "Veri1234");
         bundlesHandler = new BundlesApiHandler(env);
 
         //create a bundle
         ApiResponse createBundleResponse = bundlesHandler.doCreateBundle(eoToken, "0a79306b-7b84-4aec-8f4f-d472662cbdf2", "DELETE_ME_Automation-" + System.currentTimeMillis());
+        Assert.assertEquals(createBundleResponse.getResponseCode().intValue(), 201, "Unexpected Response code");
+
         BundlesData createdBundle = jsonPaser.fromJson(createBundleResponse.getResponseBody(), BundlesData.class);
         System.out.println("Created bundle ID = " + createdBundle.getId());
         createdBundleId = createdBundle.getId();
+
     }
 
     @AfterClass
     public void cleanUpData() throws Exception {
-
-        String eoToken = LoginHelper.getRequestToken("qa", "vfameo@getnada.com", "Veri1234");
-        bundlesHandler = new BundlesApiHandler("qa");
         ApiResponse deleteBundleResponse = bundlesHandler.doDeleteBundle(eoToken, "0a79306b-7b84-4aec-8f4f-d472662cbdf2", createdBundleId);
         System.out.println("Delete bundle response code is:" + deleteBundleResponse.getResponseCode());
 
@@ -53,11 +55,13 @@ public class PocNegativeTest extends BaseTest {
 
         bundlesHandler = new BundlesApiHandler("qa");
 
-        //create a bundle
+        //create a bundle as a merchant user
         ApiResponse createBundleResponse = bundlesHandler.doCreateBundle(merchantToken, "0a79306b-7b84-4aec-8f4f-d472662cbdf2", "DELETE_ME_Automation-" + System.currentTimeMillis());
         BundlesResponse createError = jsonParser.fromJson(createBundleResponse.getResponseBody(), BundlesResponse.class);
         System.out.println("Response code is: " + createBundleResponse.getResponseCode());
         System.out.println("Error message: " + createError.getErrors().get(0).getMessage());
+        Assert.assertEquals(createBundleResponse.getResponseCode().intValue(), 403, "Unexpected response code");
+
 
     }
 
@@ -68,12 +72,13 @@ public class PocNegativeTest extends BaseTest {
         AssignAppToBundleRequestBody assignAppsRequest = new AssignAppToBundleRequestBody(qa_free_app);
         ApiResponse assignAppResp = bundlesHandler.doAssignAppsToBundle(merchantToken, "0a79306b-7b84-4aec-8f4f-d472662cbdf2", createdBundleId, assignAppsRequest);
 
-        if (assignAppResp.getResponseCode() == 201) {
-            System.out.println("Assign apps request response code correct: " + assignAppResp.getResponseCode());
-        } else {
-            System.out.println("Wrong response code of Assign apps request: " + assignAppResp.getResponseCode());
-            System.out.println("response body " + assignAppResp.getResponseBody());
-        }
+        Assert.assertEquals(assignAppResp.getResponseCode().intValue(), 403, "Unexpected response code");
+
+        ApiResponse bundleByIdResp = bundlesHandler.getBundleById(eoToken, "0a79306b-7b84-4aec-8f4f-d472662cbdf2", createdBundleId);
+        BundlesResponse bundlesByIdRespObject = jsonParser.fromJson(bundleByIdResp.getResponseBody(), BundlesResponse.class);
+
+        Assert.assertEquals(bundlesByIdRespObject.getData().get(0).getAssignedAppmarketAppIds().size(), 0,
+                "List of assigned apps is not empty: " + bundlesByIdRespObject.getData().get(0).getAssignedAppmarketAppIds().toString());
 
     }
 
